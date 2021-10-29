95c7b523af80c535ae595afda9e6d0f4c8f1f7ac
Nazareno Casasola Angeles <nazaevantatu@hotmail.com>
2021-10-28 19:58:19 -0500

    Desacoplamiento de variables de entorno de mensajes de error

--- pom/pages/LoginPage.js
+++ pom/pages/LoginPage.js
@@ -1,13 +1,14 @@
 import { Selector, t } from 'testcafe'
+import { GENERAL_CONFING } from '../data/Constants'
 
 class LoginPage{
     constructor(){
         this.emailInput = Selector('#email')
         this.passwordInput = Selector('#password')
         this.loginButton =  Selector ('form > .submit_btn')
-        this.errorMessageInvalidEmailddress = Selector ('.error_msg > span').withExactText('Invalid email address.')
-        this.errorMessageInvalidUserOrPassword = Selector ('.error_msg > span').withExactText('Email o contraseña incorrectos')
-        this.errorMessageInvalidUserOrPasswordEnglish = Selector ('.error_msg > span').withExactText('Wrong email or password.')
+        this.errorMessageInvalidEmailddress = Selector ('.error_msg > span').withExactText(GENERAL_CONFING.TEST_CONFIG.INVALID_EMAIL_MESSAGE_ERROR_SPANISH)
+        this.errorMessageInvalidUserOrPassword = Selector ('.error_msg > span').withExactText(GENERAL_CONFING.TEST_CONFIG.INVALID_USERNAME_PASSWORD_MESSAGE_ERROR_SPANISH)
+        this.errorMessageInvalidUserOrPasswordEnglish = Selector ('.error_msg > span').withExactText(GENERAL_CONFING.TEST_CONFIG.INVALID_USERNAME_PASSWORD_MESSAGE_ERROR_SPANISH)
     }
 
     async submitLoginForm(username, password){
